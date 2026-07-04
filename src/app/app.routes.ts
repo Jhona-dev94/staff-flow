acf1b63945235bebed30b2a6907b0e5e4a539b9c
Jhona <ijonahiv@gmail.com>
2026-07-03 22:42:02 -0400

    feat: implement aplication layout with nested routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Dashboard
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                component: Dashboard
+            }
+        ]
     }
 ];

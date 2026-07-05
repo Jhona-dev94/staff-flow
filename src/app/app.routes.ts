b6a4a79b535f7ca5c7ad133aa35f58f2a3e11fd2
Jhona <ijonahiv@gmail.com>
2026-07-05 01:24:09 -0400

    feat: build responsive layout with Angular Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 import { MainLayout } from './layouts/main-layout/main-layout';
+import { Settings } from './features/dashboard/pages/settings/settings';
+import { Employees } from './features/dashboard/pages/employees/employees';
 
 export const routes: Routes = [
     {
@@ -9,7 +11,20 @@ export const routes: Routes = [
         children: [
             {
                 path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'dashboard',
                 component: Dashboard
+            },
+            {
+                path: 'settings',
+                component: Settings
+            },
+            {
+                path: 'employees',
+                component: Employees
             }
         ]
     }
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
